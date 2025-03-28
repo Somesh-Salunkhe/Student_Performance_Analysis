6ab04c1ce79f260a53dd7396ed43ebbcebba14a2
Somesh Salunkhe <salunkhesomesh1602@gmail.com>
2025-03-28 13:10:14 +0100

    Flask

--- src/pipeline/predict_pipline.py
+++ src/pipeline/predict_pipline.py
@@ -28,7 +28,7 @@ class CustomData:
     def __init__(self,
                  gender: str,
                  race_ethnicity: str,
-                 parental_level_of_eductaion: str,
+                 parental_level_of_education: str,
                  lunch: str,
                  test_preparation_course: str,
                  reading_score: int,
@@ -36,7 +36,7 @@ class CustomData:
         
         self.gender = gender
         self.race_ethnicity = race_ethnicity
-        self.parental_level_of_education = parental_level_of_eductaion
+        self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
         self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
